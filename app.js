ddf8572daec2f51d6ec7b3d4239661eac7cbadf7
Juansdv <juansdv@hotmail.com>
2023-05-07 16:33:06 -0500

    Added animation to Footer Buttons (Movile)

--- app.js
+++ app.js
@@ -1,45 +1,92 @@
-const buttonFooter = document.getElementById("abu-content-movil-1")
-const buttonFooter2 = document.getElementById("abu-content-movil-2")
-const buttonFooter3 = document.getElementById("abu-content-movil-3")
-const buttonFooter4 = document.getElementById("abu-content-movil-4")
-const buttonFooter5 = document.getElementById("abu-content-movil-5")
-const buttonFooter6 = document.getElementById("abu-content-movil-6")
-const buttonFooter7 = document.getElementById("abu-content-movil-7")
-const buttonFooter8 = document.getElementById("abu-content-movil-8")
 
-const buttonContainer = document.getElementById("abu-button-movile-1")
-const buttonContainer2 = document.getElementById("abu-button-movile-2")
-const buttonContainer3 = document.getElementById("abu-button-movile-3")
-const buttonContainer4 = document.getElementById("abu-button-movile-4")
-const buttonContainer5 = document.getElementById("abu-button-movile-5")
-const buttonContainer6 = document.getElementById("abu-button-movile-6")
-const buttonContainer7 = document.getElementById("abu-button-movile-7")
-const buttonContainer8 = document.getElementById("abu-button-movile-8")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function EfectoBoton(){
+    const buttonFooter = document.getElementById("abu-content-movil-1");
+    const buttonContainer = document.getElementById("abu-button-movile-1");
+    const spanButton = document.getElementById("abu-span-movile-1");
 buttonContainer.addEventListener("click", () =>{
-    buttonFooter.classList.toggle('efectoBoton')
-}),
+    buttonFooter.classList.toggle('efectoBoton');
+    spanButton.style.transition="all 0.3s ease-in";
+    spanButton.classList.toggle('span-boton');
+});
+
+    const buttonFooter2 = document.getElementById("abu-content-movil-2");
+    const buttonContainer2 = document.getElementById("abu-button-movile-2");
+    const spanButton2 = document.getElementById("abu-span-movile-2");
 buttonContainer2.addEventListener("click", () => {
     buttonFooter2.classList.toggle("efectoBoton")
-}),
+    spanButton2.style.transition="all 0.3s ease-in";
+    spanButton2.classList.toggle('span-boton');
+});
+
+    const buttonFooter3 = document.getElementById("abu-content-movil-3")
+    const buttonContainer3 = document.getElementById("abu-button-movile-3")
+    const spanButton3 = document.getElementById("abu-span-movile-3");
 buttonContainer3.addEventListener("click", () => {
     buttonFooter3.classList.toggle("efectoBoton")
-}),
+
+});
+
+    const buttonFooter4 = document.getElementById("abu-content-movil-4")
+    const buttonContainer4 = document.getElementById("abu-button-movile-4")
+    const spanButton4 = document.getElementById("abu-span-movile-4");
 buttonContainer4.addEventListener("click", () => {
     buttonFooter4.classList.toggle("efectoBoton")
-}),
+    spanButton4.style.transition="all 0.3s ease-in";
+    spanButton4.classList.toggle('span-boton');
+});
+
+    const buttonFooter5 = document.getElementById("abu-content-movil-5")
+    const buttonContainer5 = document.getElementById("abu-button-movile-5")
+    const spanButton5 = document.getElementById("abu-span-movile-5");
 buttonContainer5.addEventListener("click", () => {
     buttonFooter5.classList.toggle("efectoBoton")
-}),
+    spanButton5.style.transition="all 0.3s ease-in";
+    spanButton5.classList.toggle('span-boton');
+});
+
+    const buttonFooter6 = document.getElementById("abu-content-movil-6")
+    const buttonContainer6 = document.getElementById("abu-button-movile-6")
+    const spanButton6 = document.getElementById("abu-span-movile-6");
 buttonContainer6.addEventListener("click", () => {
     buttonFooter6.classList.toggle("efectoBoton")
-}),
+    spanButton6.style.transition="all 0.3s ease-in";
+    spanButton6.classList.toggle('span-boton');
+});
+
+    const buttonFooter7 = document.getElementById("abu-content-movil-7")
+    const buttonContainer7 = document.getElementById("abu-button-movile-7")
+    const spanButton7 = document.getElementById("abu-span-movile-7");
 buttonContainer7.addEventListener("click", () => {
     buttonFooter7.classList.toggle("efectoBoton")
-}),
+    spanButton7.style.transition="all 0.3s ease-in";
+    spanButton7.classList.toggle('span-boton');
+});
+
+    const buttonFooter8 = document.getElementById("abu-content-movil-8")
+    const buttonContainer8 = document.getElementById("abu-button-movile-8")
+    const spanButton8 = document.getElementById("abu-span-movile-8");
+
 buttonContainer8.addEventListener("click", () => {
     buttonFooter8.classList.toggle("efectoBoton")
+    spanButton8.style.transition="all 0.3s ease-in";
+    spanButton8.classList.toggle('span-boton');
 })
 
 }
